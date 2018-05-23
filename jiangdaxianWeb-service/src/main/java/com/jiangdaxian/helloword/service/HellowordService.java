ce37d05b32f83bd2068ebf3122c2a49636edd350
DESKTOP-LDB44O7\dell <15914380151@139.com>
2018-05-23 18:43:27 +0800

    dubbo

--- jiangdaxianWeb-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
+++ jiangdaxianWeb-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
@@ -1,10 +1,18 @@
 package com.jiangdaxian.helloword.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jiangdaxian.helloword.api.HellowordApi;
 
 @Service
 public class HellowordService {
+	@Autowired
+	private HellowordApi hellowordApi;
+	
 	public String get() {
-		return "jdxTest:"+System.currentTimeMillis();
+		String dubboApi = hellowordApi.sayHello();
+		
+		return "jdxTest:"+System.currentTimeMillis() +","+dubboApi;
 	}
 }
