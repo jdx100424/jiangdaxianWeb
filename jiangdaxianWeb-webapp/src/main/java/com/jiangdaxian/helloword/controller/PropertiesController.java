cdd5b3b26e6283662cfc0bf91320a37e2da51c49
DESKTOP-LDB44O7\dell <15914380151@139.com>
2018-05-25 14:25:21 +0800

    ehcache

--- jiangdaxianWeb-webapp/src/main/java/com/jiangdaxian/helloword/controller/PropertiesController.java
+++ jiangdaxianWeb-webapp/src/main/java/com/jiangdaxian/helloword/controller/PropertiesController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,12 +18,25 @@ import com.jiangdaxian.common.util.properties.PropertiesUtils;
 public class PropertiesController {
 	private static final Logger LOG = LoggerFactory.getLogger(PropertiesController.class);
 	
+	@Autowired
+	private EhCacheCacheManager ehCacheCacheManager;
+	
+	private static final String CACHE_NAME = "hellowordCache";
+	
 	@RequestMapping("/properties")
 	@ResponseBody
 	public String index() {
 		Map<String, Object> map = new HashMap<String, Object>();
 		map.put("zookeeper.url",PropertiesUtils.getProperty("zookeeper.url"));
 		LOG.info(JSONObject.toJSONString(map));
+		
+		Object obj = ehCacheCacheManager.getCache(CACHE_NAME).get("jdx");
+		if(obj==null) {
+			LOG.warn(CACHE_NAME + ",not data");
+			ehCacheCacheManager.getCache(CACHE_NAME).put("jdx", "1111");
+		}else {
+			LOG.warn(CACHE_NAME + ",data");
+		}
 		return JSONObject.toJSONString(map);
 	}
 }
