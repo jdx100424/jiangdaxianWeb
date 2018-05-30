97b61f8e44be641df5bd0c649af6217038c9cc47
DESKTOP-LDB44O7\dell <15914380151@139.com>
2018-05-30 10:38:54 +0800

    增加dubbo过滤器

--- jiangdaxianWeb-webapp/src/main/java/com/jiangdaxian/base/WebApplication.java
+++ jiangdaxianWeb-webapp/src/main/java/com/jiangdaxian/base/WebApplication.java
@@ -2,12 +2,14 @@ package com.jiangdaxian.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.jiangdaxian" })
 @ImportResource({ "classpath:applicationContext.xml" })
+@ServletComponentScan(basePackages={"com.jiangdaxian.web.filter"}) 
 public class WebApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(WebApplication.class, args);
