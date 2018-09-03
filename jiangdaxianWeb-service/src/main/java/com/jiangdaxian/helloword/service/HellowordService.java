9ea52bc50d480f84e50e3e48fa6bf45e44de8549
jdx100424 <15914380151@139.com>
2018-09-03 13:41:12 +0800

    1111

--- jiangdaxianWeb-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
+++ jiangdaxianWeb-service/src/main/java/com/jiangdaxian/helloword/service/HellowordService.java
@@ -8,22 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
-import com.jiangdaxian.helloword.api.HellowordApi;
-//import com.jiangdaxian.kafka.BaseProducer;
-//import com.jiangdaxian.kafka.dto.MessageDto;
-
 @Service
 public class HellowordService {
-	@Autowired
-	private HellowordApi hellowordApi;
+
 	/**
 	@Autowired
 	@Qualifier("hellowordProducer")
 	private com.jiangdaxian.kafka.BaseProducer<Calendar> hellowordProducer;*/
 	
 	public String get() {
-		String dubboApi = hellowordApi.sayHello();
-		return "jdxTest:"+System.currentTimeMillis() +","+dubboApi;
+		
+		return "jdxTest:"+System.currentTimeMillis() ;
 	}
 	
 	public void sendKafka() {
--- jiangdaxianWeb-webapp/src/main/java/com/jiangdaxian/helloword/controller/PropertiesController.java
+++ jiangdaxianWeb-webapp/src/main/java/com/jiangdaxian/helloword/controller/PropertiesController.java
@@ -6,20 +6,16 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.fastjson.JSONObject;
-import com.jiangdaxian.common.util.properties.PropertiesUtils;
 
 @Controller
 public class PropertiesController {
 	private static final Logger LOG = LoggerFactory.getLogger(PropertiesController.class);
-	
-	@Autowired
-	private EhCacheCacheManager ehCacheCacheManager;
+
 	
 	private static final String CACHE_NAME = "hellowordCache";
 	
@@ -27,16 +23,9 @@ public class PropertiesController {
 	@ResponseBody
 	public String index() {
 		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("zookeeper.url",PropertiesUtils.getProperty("zookeeper.url"));
+		map.put("zookeeper.url","dddddddddddddddd");
 		LOG.info(JSONObject.toJSONString(map));
-		
-		Object obj = ehCacheCacheManager.getCache(CACHE_NAME).get("jdx");
-		if(obj==null) {
-			LOG.warn(CACHE_NAME + ",not data");
-			ehCacheCacheManager.getCache(CACHE_NAME).put("jdx", "1111");
-		}else {
-			LOG.warn(CACHE_NAME + ",data");
-		}
+	
 		return JSONObject.toJSONString(map);
 	}
 }
